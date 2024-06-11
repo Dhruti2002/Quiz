98f6bebabf23573542ed73af45b1786ec454d246
Dhruti Patel <132155248+Dhruti2002@users.noreply.github.com>
2024-06-11 12:03:09 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "The World Wide Web (WWW) is",
+        answer: "B. A collection of interconnected websites and documents.",
+        options: [
+            "A. The same as the internet.",
+            "B. A collection of interconnected websites and documents.",
+            "C. A type of computer network.",
+            "D.  A social media platform."
+        ]
+    },
+    {
+        numb: 2,
+        question: "A program that translates high-level code into machine code is called a:",
+        answer: "A. Compiler",
+        options: [
+            "A. Compiler",
+            "B. Interpreter",
+            "C. Assembler",
+            "D. Debugger"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which of the following is NOT a primary storage device?",
+        answer: "D. Optical Drive",
+        options: [
+            "A. Hard Disk Drive (HDD)",
+            "B. Random Access Memory (RAM)",
+            "C. Solid State Drive (SSD)",
+            "D. Optical Drive"
+        ]
+    },
+    {
+        numb: 4,
+        question: "In a computer network, what device connects devices and allows them to communicate?",
+        answer: "B. Router",
+        options: [
+            "A. Monitor",
+            "B. Router",
+            "C. Keyboard",
+            "D. Printer"
+        ]
+    },
+    {
+        numb: 5,
+        question: "The process of searching for and fixing errors in a program is called:",
+        answer: "A. Debugging",
+        options: [
+            "A. Debugging",
+            "B. Compiling",
+            "C. Uploading",
+            "D. Downloading"
+        ]
+    },
+    {
+        numb: 6,
+        question: "The most common type of cyberattack that steals personal information is called:",
+        answer: "A. Phishing",
+        options: [
+            "A. Phishing",
+            "B. Denial-of-Service",
+            "C. Malware",
+            "D. Virus"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Cloud computing refers to:",
+        answer: "C. Storing and accessing data and programs over the internet.",
+        options: [
+            "A. Using a computer with a physical keyboard and mouse.",
+            "B. Uploading large files to a website.",
+            "C. Storing and accessing data and programs over the internet.",
+            "D. Downloading software to your computer."
+        ]
+    },
+    {
+        numb: 8,
+        question: "In programming, a loop is used to:",
+        answer: "D. Repeat a block of code a specific number of times.",
+        options: [
+            "A. Print a document.",
+            "B. Display a message on the screen.",
+            "C. Store data in a variable.",
+            "D. Repeat a block of code a specific number of times."
+        ]
+    },
+    {
+        numb: 9,
+        question: "The inventor of the World Wide Web is:",
+        answer: "C. Tim Berners-Lee",
+        options: [
+            "A. Steve Jobs",
+            "B. Bill Gates",
+            "C. Tim Berners-Lee",
+            "D. Mark Zuckerberg"
+        ]
+    },
+    {
+        numb: 10,
+        question: "What is the name of the logic behind search engines like Google?",
+        answer: "B. Algorithms",
+        options: [
+            "A. Artificial Intelligence (AI)",
+            "B. Algorithms",
+            "C.  Binary code",
+            "D. Machine Learning"
+        ]
+    }
+];
